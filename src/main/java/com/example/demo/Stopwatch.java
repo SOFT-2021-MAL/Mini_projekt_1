947da2bdeb5e3a0431fa0aa58ff9e2492d2cdbdb
CONVERGENS\mak <mak@convergens.lan>
2021-02-22 10:03:57 +0100

    first

---
+++ src/main/java/com/example/demo/Stopwatch.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.io.PrintStream;
+
+public class Stopwatch implements AutoCloseable{
+    private final PrintStream out;
+    private final long nanos = System . nanoTime ();
+    public Stopwatch ( PrintStream out) { this .out = out; }
+    public Stopwatch () { this ( System .out ); }
+    public double step () {
+        return ( System . nanoTime () - nanos )/1000000.0;
+    }
+    @Override
+    public void close () {
+        out. printf (" %5.6 f\n", step ());
+    }
+}
